1dd9d064a859a5d217f3c00eeda46ccdf6540002
Yeremy <yerethedark@gmail.com>
2026-02-08 19:37:58 -0400

    cambiando main.jsx y package.json

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import { StrictMode } from "react";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter  } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
@@ -7,8 +7,8 @@ import "./i18n";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter >
       <App />
-    </BrowserRouter>
+    </HashRouter >
   </StrictMode>
 );
